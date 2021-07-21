91b4b4d2d0fba2e690e890b616cfcad819a86c5e
FranzMan <thefranzman@googlemail.com>
2021-07-21 11:19:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,13 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
-        basic.showIcon(IconNames.No)
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        x = value
     }
-    if (receivedNumber == 1) {
-        calliBot2.motor(C2Motor.beide, C2Dir.vorw&#228;rts, 50)
-        basic.showIcon(IconNames.ArrowSouth)
+    if (name == "y") {
+        y = value
     }
-    if (receivedNumber == 2) {
-        calliBot2.motor(C2Motor.beide, C2Dir.r&#252;ckw&#228;rts, 50)
-        basic.showIcon(IconNames.ArrowNorth)
-    }
-    if (receivedNumber == 3) {
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorw&#228;rts, 50)
-        calliBot2.motorStop(C2Motor.links, C2Stop.Bremsen)
-        basic.showString("L")
-    }
-    if (receivedNumber == 4) {
-        calliBot2.motor(C2Motor.links, C2Dir.vorw&#228;rts, 50)
-        calliBot2.motorStop(C2Motor.rechts, C2Stop.Bremsen)
-        basic.showString("R")
-    }
+    basic.showNumber(x)
+    basic.showNumber(y)
 })
+let y = 0
+let x = 0
 radio.setGroup(13)
